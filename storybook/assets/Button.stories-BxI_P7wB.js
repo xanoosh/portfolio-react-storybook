445002afc8b34196c0c3591247a05d9d6740a8ad
xanoosh <xanoosh@gmail.com>
2024-04-02 11:40:22 +0200

    button icon stories

---
+++ storybook/assets/Button.stories-BxI_P7wB.js
@@ -0,0 +1,55 @@
+import{B as j}from"./Button-1COFxq4a.js";import"./jsx-runtime-BnIj46N_.js";import"./index-CsdIBAqE.js";const A={title:"Portfolio/Button",component:j,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{text:"example primary button text",url:"#",variant:"primary"}},e={args:{text:"example secondary button text",url:"#",variant:"secondary"}},a={args:{text:"example disabled primary button text",url:"#",variant:"primary",disabled:!0}},t={args:{text:"example disabled secondary button text",url:"#",variant:"secondary",disabled:!0}},n={args:{text:"example primary button with web icon",url:"#",variant:"primary",icon:"webIcon"}},o={args:{text:"example primary button with web icon",url:"#",variant:"primary",icon:"codeIcon"}},s={args:{text:"example secondary button with web icon",url:"#",variant:"secondary",icon:"webIcon"}},c={args:{text:"example secondary button with web icon",url:"#",variant:"secondary",icon:"codeIcon"}};var i,d,u;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    text: 'example primary button text',
+    url: '#',
+    variant: 'primary'
+  }
+}`,...(u=(d=r.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var m,p,l;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    text: 'example secondary button text',
+    url: '#',
+    variant: 'secondary'
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var y,b,x;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    text: 'example disabled primary button text',
+    url: '#',
+    variant: 'primary',
+    disabled: true
+  }
+}`,...(x=(b=a.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var g,w,B;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    text: 'example disabled secondary button text',
+    url: '#',
+    variant: 'secondary',
+    disabled: true
+  }
+}`,...(B=(w=t.parameters)==null?void 0:w.docs)==null?void 0:B.source}}};var v,I,S;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    text: 'example primary button with web icon',
+    url: '#',
+    variant: 'primary',
+    icon: 'webIcon'
+  }
+}`,...(S=(I=n.parameters)==null?void 0:I.docs)==null?void 0:S.source}}};var P,h,C;o.parameters={...o.parameters,docs:{...(P=o.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    text: 'example primary button with web icon',
+    url: '#',
+    variant: 'primary',
+    icon: 'codeIcon'
+  }
+}`,...(C=(h=o.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};var D,W,f;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    text: 'example secondary button with web icon',
+    url: '#',
+    variant: 'secondary',
+    icon: 'webIcon'
+  }
+}`,...(f=(W=s.parameters)==null?void 0:W.docs)==null?void 0:f.source}}};var _,E,O;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    text: 'example secondary button with web icon',
+    url: '#',
+    variant: 'secondary',
+    icon: 'codeIcon'
+  }
+}`,...(O=(E=c.parameters)==null?void 0:E.docs)==null?void 0:O.source}}};const F=["ButtonPrimary","ButtonSecondary","DisabledButtonPrimary","DisabledButtonSecondary","ButtonPrimaryWebIcon","ButtonPrimaryCodeIcon","ButtonSecondaryWebIcon","ButtonSecondaryCodeIcon"];export{r as ButtonPrimary,o as ButtonPrimaryCodeIcon,n as ButtonPrimaryWebIcon,e as ButtonSecondary,c as ButtonSecondaryCodeIcon,s as ButtonSecondaryWebIcon,a as DisabledButtonPrimary,t as DisabledButtonSecondary,F as __namedExportsOrder,A as default};
