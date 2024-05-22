95ce0802030ab6c1277c394c7fd7537f54256636
xanoosh <xanoosh@gmail.com>
2024-05-22 13:55:55 +0200

    storybook badge update

--- storybook/assets/Navbar.stories-B1mesuno.js
+++ storybook/assets/Navbar.stories-B1mesuno.js
@@ -1,4 +1,4 @@
-import{r as g,a as en,R as Z}from"./index-CsdIBAqE.js";import{R as tn}from"./index-CpXDz0sX.js";import{j as te,w as rn,r as nn}from"./jsx-runtime-BMEMobkm.js";import"./tiny-invariant-CopsF_GD.js";/**
+import{r as g,a as en,R as Z}from"./index-CsdIBAqE.js";import{R as tn}from"./index-CpXDz0sX.js";import{j as te,w as rn,r as nn}from"./jsx-runtime-CPoh4Ics.js";import"./tiny-invariant-CopsF_GD.js";/**
  * @remix-run/router v1.15.3
  *
  * Copyright (c) Remix Software Inc.
