2d38df2eec7661eb34254176c863b907a4148ac1
xanoosh <xanoosh@gmail.com>
2024-05-29 10:23:33 +0200

    upgrade to v8.1.4, TooltipComponent stories

---
+++ storybook/assets/Badge.stories-B9QS5PSj.js
@@ -0,0 +1,49 @@
+import{w as E,r as O}from"./jsx-runtime-BlHMf7ja.js";import{B as T}from"./Badge-BgobKMiV.js";import"./tiny-invariant-CopsF_GD.js";import"./index-CsdIBAqE.js";const D={title:"Portfolio/Badge",component:T,parameters:{layout:"centered"},decorators:[E({results:O})],tags:["autodocs"]},e={args:{text:"small pink badge text",variant:"pink",size:"sm"}},a={args:{text:"large pink badge text",variant:"pink",size:"lg"}},t={args:{text:"small main badge text",size:"sm"}},r={args:{text:"large main badge text",size:"lg"}},o={args:{text:"small pink badge with remove button text",variant:"pink",size:"sm",handleRemove:()=>console.log("remove")}},s={args:{text:"large pink badge with remove button text",variant:"pink",size:"lg",handleRemove:()=>console.log("remove")}},n={args:{text:"small main badge with remove button text",size:"sm",handleRemove:()=>console.log("remove")}},m={args:{text:"large main badge with remove button text",size:"lg",handleRemove:()=>console.log("remove")}};var i,g,l;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    text: 'small pink badge text',
+    variant: 'pink',
+    size: 'sm'
+  }
+}`,...(l=(g=e.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var d,c,p;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    text: 'large pink badge text',
+    variant: 'pink',
+    size: 'lg'
+  }
+}`,...(p=(c=a.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var u,v,x;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    text: 'small main badge text',
+    size: 'sm'
+  }
+}`,...(x=(v=t.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var B,h,b;r.parameters={...r.parameters,docs:{...(B=r.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    text: 'large main badge text',
+    size: 'lg'
+  }
+}`,...(b=(h=r.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var k,z,R;o.parameters={...o.parameters,docs:{...(k=o.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    text: 'small pink badge with remove button text',
+    variant: 'pink',
+    size: 'sm',
+    handleRemove: () => console.log('remove')
+  }
+}`,...(R=(z=o.parameters)==null?void 0:z.docs)==null?void 0:R.source}}};var S,w,P;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    text: 'large pink badge with remove button text',
+    variant: 'pink',
+    size: 'lg',
+    handleRemove: () => console.log('remove')
+  }
+}`,...(P=(w=s.parameters)==null?void 0:w.docs)==null?void 0:P.source}}};var L,M,W;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    text: 'small main badge with remove button text',
+    size: 'sm',
+    handleRemove: () => console.log('remove')
+  }
+}`,...(W=(M=n.parameters)==null?void 0:M.docs)==null?void 0:W.source}}};var f,_,y;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    text: 'large main badge with remove button text',
+    size: 'lg',
+    handleRemove: () => console.log('remove')
+  }
+}`,...(y=(_=m.parameters)==null?void 0:_.docs)==null?void 0:y.source}}};const F=["SmallPinkBadge","LargePinkBadge","SmallMainBadge","LargeMainBadge","SmallPinkBadgeWithRemoveButton","LargePinkBadgeWithRemoveButton","SmallMainBadgeWithRemoveButton","LargeMainBadgeWithRemoveButton"];export{r as LargeMainBadge,m as LargeMainBadgeWithRemoveButton,a as LargePinkBadge,s as LargePinkBadgeWithRemoveButton,t as SmallMainBadge,n as SmallMainBadgeWithRemoveButton,e as SmallPinkBadge,o as SmallPinkBadgeWithRemoveButton,F as __namedExportsOrder,D as default};
---
+++ storybook/assets/Button.stories-e9agU6Mz.js
@@ -0,0 +1,82 @@
+import{w as U,r as V}from"./jsx-runtime-BlHMf7ja.js";import{B as X}from"./Button-B4oMzjFl.js";import"./tiny-invariant-CopsF_GD.js";import"./index-CsdIBAqE.js";const re={title:"Portfolio/Button",component:X,parameters:{layout:"centered"},decorators:[U({results:V})],tags:["autodocs"]},e={args:{text:"example primary button text",url:"#",variant:"primary"}},r={args:{text:"example pink button text",url:"#",variant:"pink"}},t={args:{text:"example secondary button text",url:"#",variant:"secondary"}},a={args:{text:"example disabled primary button text",url:"#",variant:"primary",disabled:!0}},n={args:{text:"example disabled pink button text",url:"#",variant:"pink",disabled:!0}},o={args:{text:"example disabled secondary button text",url:"#",variant:"secondary",disabled:!0}},s={args:{text:"example primary button with web icon",url:"#",variant:"primary",icon:"webIcon"}},c={args:{text:"example primary button with code icon",url:"#",variant:"primary",icon:"codeIcon"}},i={args:{text:"example pink button with web icon",url:"#",variant:"pink",icon:"webIcon"}},u={args:{text:"example pink button with code icon",url:"#",variant:"pink",icon:"codeIcon"}},d={args:{text:"example secondary button with web icon",url:"#",variant:"secondary",icon:"webIcon"}},p={args:{text:"example secondary button with code icon",url:"#",variant:"secondary",icon:"codeIcon"}};var m,l,x;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    text: 'example primary button text',
+    url: '#',
+    variant: 'primary'
+  }
+}`,...(x=(l=e.parameters)==null?void 0:l.docs)==null?void 0:x.source}}};var b,y,g;r.parameters={...r.parameters,docs:{...(b=r.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    text: 'example pink button text',
+    url: '#',
+    variant: 'pink'
+  }
+}`,...(g=(y=r.parameters)==null?void 0:y.docs)==null?void 0:g.source}}};var B,w,k;t.parameters={...t.parameters,docs:{...(B=t.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    text: 'example secondary button text',
+    url: '#',
+    variant: 'secondary'
+  }
+}`,...(k=(w=t.parameters)==null?void 0:w.docs)==null?void 0:k.source}}};var v,I,S;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    text: 'example disabled primary button text',
+    url: '#',
+    variant: 'primary',
+    disabled: true
+  }
+}`,...(S=(I=a.parameters)==null?void 0:I.docs)==null?void 0:S.source}}};var P,h,C;n.parameters={...n.parameters,docs:{...(P=n.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    text: 'example disabled pink button text',
+    url: '#',
+    variant: 'pink',
+    disabled: true
+  }
+}`,...(C=(h=n.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};var D,W,f;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    text: 'example disabled secondary button text',
+    url: '#',
+    variant: 'secondary',
+    disabled: true
+  }
+}`,...(f=(W=o.parameters)==null?void 0:W.docs)==null?void 0:f.source}}};var _,E,O;s.parameters={...s.parameters,docs:{...(_=s.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    text: 'example primary button with web icon',
+    url: '#',
+    variant: 'primary',
+    icon: 'webIcon'
+  }
+}`,...(O=(E=s.parameters)==null?void 0:E.docs)==null?void 0:O.source}}};var T,j,q;c.parameters={...c.parameters,docs:{...(T=c.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    text: 'example primary button with code icon',
+    url: '#',
+    variant: 'primary',
+    icon: 'codeIcon'
+  }
+}`,...(q=(j=c.parameters)==null?void 0:j.docs)==null?void 0:q.source}}};var z,A,F;i.parameters={...i.parameters,docs:{...(z=i.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    text: 'example pink button with web icon',
+    url: '#',
+    variant: 'pink',
+    icon: 'webIcon'
+  }
+}`,...(F=(A=i.parameters)==null?void 0:A.docs)==null?void 0:F.source}}};var G,H,J;u.parameters={...u.parameters,docs:{...(G=u.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    text: 'example pink button with code icon',
+    url: '#',
+    variant: 'pink',
+    icon: 'codeIcon'
+  }
+}`,...(J=(H=u.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};var K,L,M;d.parameters={...d.parameters,docs:{...(K=d.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    text: 'example secondary button with web icon',
+    url: '#',
+    variant: 'secondary',
+    icon: 'webIcon'
+  }
+}`,...(M=(L=d.parameters)==null?void 0:L.docs)==null?void 0:M.source}}};var N,Q,R;p.parameters={...p.parameters,docs:{...(N=p.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    text: 'example secondary button with code icon',
+    url: '#',
+    variant: 'secondary',
+    icon: 'codeIcon'
+  }
+}`,...(R=(Q=p.parameters)==null?void 0:Q.docs)==null?void 0:R.source}}};const te=["ButtonPrimary","ButtonPink","ButtonSecondary","DisabledButtonPrimary","DisabledButtonPink","DisabledButtonSecondary","ButtonPrimaryWebIcon","ButtonPrimaryCodeIcon","ButtonPinkWebIcon","ButtonPinkCodeIcon","ButtonSecondaryWebIcon","ButtonSecondaryCodeIcon"];export{r as ButtonPink,u as ButtonPinkCodeIcon,i as ButtonPinkWebIcon,e as ButtonPrimary,c as ButtonPrimaryCodeIcon,s as ButtonPrimaryWebIcon,t as ButtonSecondary,p as ButtonSecondaryCodeIcon,d as ButtonSecondaryWebIcon,n as DisabledButtonPink,a as DisabledButtonPrimary,o as DisabledButtonSecondary,te as __namedExportsOrder,re as default};
