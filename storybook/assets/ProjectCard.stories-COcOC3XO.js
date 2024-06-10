d0439a115e75be3b0f53a56aaf75d94dcfdf19b7
xanoosh <xanoosh@gmail.com>
2024-06-11 00:20:57 +0200

    projectfilter story update

---
+++ storybook/assets/ProjectCard.stories-COcOC3XO.js
@@ -0,0 +1,48 @@
+import{j as t,w as E,r as B}from"./jsx-runtime-C4D8mA-b.js";import{m as D,B as I}from"./Badge-BHCUWRVA.js";import{B as O}from"./Button-DWJkVP_0.js";import{T as R}from"./TooltipComponent-DLNo-WDo.js";import"./tiny-invariant-CopsF_GD.js";import"./index-CsdIBAqE.js";import"./index-D1uLUWWR.js";import"./index-CpXDz0sX.js";function c({title:d,description:p,badges:u,activeBadges:e,buttons:m,handleBadgeClick:g}){return t.jsxs(D.div,{initial:{opacity:0},animate:{opacity:1},exit:{opacity:0},transition:{duration:.2},className:"flex flex-col gap-4 backdrop-blur-sm p-6 rounded-lg shadow bg-slate-800/50",children:[d?t.jsx("h3",{className:"text-2xl font-bold tracking-tight text-white",children:d}):null,p?t.jsx("p",{className:"font-normal text-slate-300",children:p}):null,u?t.jsx("div",{className:"flex flex-row gap-2 flex-wrap",children:u.map((r,l)=>t.jsx(R,{text:e!=null&&e.includes(r)?"Click to remove this item from taglist":"Click to add this item to taglist",children:t.jsx(I,{text:r,variant:e!=null&&e.includes(r)?"pink":"main",handleClick:g?()=>g(r):()=>null})},l))}):null,m?t.jsx("div",{className:"flex flex-row sm:flex-nowrap flex-wrap mt-auto gap-4",children:m.map(({text:r,url:l,icon:S},T)=>t.jsx(O,{text:r,url:l,icon:S},T))}):null]})}try{c.displayName="ProjectCard",c.__docgenInfo={description:"",displayName:"ProjectCard",props:{title:{defaultValue:null,description:"",name:"title",required:!1,type:{name:"string"}},description:{defaultValue:null,description:"",name:"description",required:!1,type:{name:"string"}},badges:{defaultValue:null,description:"",name:"badges",required:!0,type:{name:"string[]"}},activeBadges:{defaultValue:null,description:"",name:"activeBadges",required:!1,type:{name:"string[]"}},buttons:{defaultValue:null,description:"",name:"buttons",required:!1,type:{name:"ButtonProps[]"}},handleBadgeClick:{defaultValue:null,description:"",name:"handleBadgeClick",required:!1,type:{name:"((badgeName: string) => void)"}}}}}catch{}const M={title:"Portfolio/ProjectCard",component:c,parameters:{layout:"centered",backgrounds:{default:"dark"}},decorators:[E({results:B})],tags:["autodocs"]},a={args:{title:"example title",description:"example project card description",badges:["badge1","badge2"],buttons:[{text:"example button",url:""}]}},o={args:{title:"",description:"project card without title",badges:["badge1","badge2"],buttons:[{text:"example button",url:""}]}},s={args:{title:"project card without description",description:"",badges:["badge1","badge2"],buttons:[{text:"example button",url:""}]}},n={args:{title:"project card without badges",description:"this is project card without badges",badges:[],buttons:[{text:"example button",url:""}]}},i={args:{title:"project card without buttons",description:"this is project card without buttons",badges:["badge1","badge2"],buttons:[]}};var b,x,h;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    title: 'example title',
+    description: 'example project card description',
+    badges: ['badge1', 'badge2'],
+    buttons: [{
+      text: 'example button',
+      url: ''
+    }]
+  }
+}`,...(h=(x=a.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var j,f,w;o.parameters={...o.parameters,docs:{...(j=o.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    title: '',
+    description: 'project card without title',
+    badges: ['badge1', 'badge2'],
+    buttons: [{
+      text: 'example button',
+      url: ''
+    }]
+  }
+}`,...(w=(f=o.parameters)==null?void 0:f.docs)==null?void 0:w.source}}};var C,P,y;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    title: 'project card without description',
+    description: '',
+    badges: ['badge1', 'badge2'],
+    buttons: [{
+      text: 'example button',
+      url: ''
+    }]
+  }
+}`,...(y=(P=s.parameters)==null?void 0:P.docs)==null?void 0:y.source}}};var _,k,N;n.parameters={...n.parameters,docs:{...(_=n.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    title: 'project card without badges',
+    description: 'this is project card without badges',
+    badges: [],
+    buttons: [{
+      text: 'example button',
+      url: ''
+    }]
+  }
+}`,...(N=(k=n.parameters)==null?void 0:k.docs)==null?void 0:N.source}}};var W,q,V;i.parameters={...i.parameters,docs:{...(W=i.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    title: 'project card without buttons',
+    description: 'this is project card without buttons',
+    badges: ['badge1', 'badge2'],
+    buttons: []
+  }
+}`,...(V=(q=i.parameters)==null?void 0:q.docs)==null?void 0:V.source}}};const Q=["ExampleProjectCard","ProjectCardWithoutTitle","ProjectCardWithoutDescription","ProjectCardWithoutBadges","ProjectCardWithoutButtons"];export{a as ExampleProjectCard,n as ProjectCardWithoutBadges,i as ProjectCardWithoutButtons,s as ProjectCardWithoutDescription,o as ProjectCardWithoutTitle,Q as __namedExportsOrder,M as default};
---
+++ storybook/assets/preview-C7h2B_5g.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-K4EAMTCU-vB4UxN8K.js","./iframe-BB0R21Ah.js","./index-CsdIBAqE.js","./react-18-BLPBl96z.js","./index-CpXDz0sX.js","./index-D1uLUWWR.js","./index-DYADbu9O.js","./_getPrototype-x0OssJAz.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./iframe-BB0R21Ah.js";import"../sb-preview/runtime.js";const{global:s}=__STORYBOOK_MODULE_GLOBAL__;var _=Object.entries(s.TAGS_OPTIONS??{}).reduce((e,r)=>{let[t,o]=r;return o.excludeFromDocsStories&&(e[t]=!0),e},{}),n={docs:{renderer:async()=>{let{DocsRenderer:e}=await a(()=>import("./DocsRenderer-K4EAMTCU-vB4UxN8K.js").then(r=>r.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8]),import.meta.url);return new e},stories:{filter:e=>{var r;return(e.tags||[]).filter(t=>_[t]).length===0&&!((r=e.parameters.docs)!=null&&r.disable)}}}};export{n as parameters};
